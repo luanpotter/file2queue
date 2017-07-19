bc82cef6fb490da824417f16e1dd76af311bd4c5
Luan Nico <luanpotter27@gmail.com>
2017-07-19 16:12:02 -0300

    Base

---
+++ src/main/java/xyz/luan/file2queue/App.java
@@ -0,0 +1,13 @@
+package xyz.luan.file2queue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
